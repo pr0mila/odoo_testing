ec9e268afae2e587a8ed65b407632b55c5558d76
Monty <me@promila.info>
2022-06-10 11:57:22 +0600

    custom module

---
+++ school_management/__manifest__.py
@@ -0,0 +1,23 @@
+
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'School Management',
+    'version' : '1.0',
+    'summary': 'School Management and Maintenance',
+    'sequence': 10,
+    'description': """
+    School Management and Maintenance
+    """,
+    'category': 'Human Resources/Employees',
+    'website': 'https://promila.info/',
+    'images' : [],
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
