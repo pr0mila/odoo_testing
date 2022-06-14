2d0487760b164e04c2085c432a55af7427f74d6b
Monty <me@promila.info>
2022-06-14 22:53:28 +0600

    Can't solve birthday issue

--- school_management/student.py
+++ school_management/student.py
@@ -1,15 +1,18 @@
+from dateutil import parser
 from datetime import datetime, time, date
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
 
 from odoo import fields, models
 from odoo.fields import Date
 
 
 def _check_dates(dob):
-    today = Date.today()
-    if dob <= today:
-        return dob
+    d1 = datetime.strptime(str(dob), "%Y/%m/%d").date()
+    d2 = date.today()
+    if d1 > d2:
+        return d2
     else:
-        return today
+        d1
 
 
 class Student(models.Model):
@@ -28,9 +31,10 @@ class Student(models.Model):
                          "The User ID must be unique, this one is already assigned to another user."),
 
                         ]
+    #_constraints = [(_check_dates(dob), 'Error ! Date must be less than Current Date')]
 
 
-#  _constraints = [(_check_dates(dob), 'Error ! Date must be less than Current Date')]
+
 
 
 class Class(models.Model):
