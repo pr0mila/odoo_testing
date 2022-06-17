cd56c47180362f4086ffc921f3a53f8ac26cd9d2
Monty <me@promila.info>
2022-06-18 00:58:13 +0600

    stared to one to many field of teacher

--- school_management/teacher.py
+++ school_management/teacher.py
@@ -4,7 +4,10 @@ from odoo import fields, models
 class Teacher(models.Model):
     _inherit = "hr.employee"
     teacher_name_designation = fields.Char(string="Designation")
+    subject_name = fields.One2many('teacher.class', 'class_name', string="Classes")
 
 
-
-
+class Class(models.Model):
+    _name = 'teacher.class'
+    _description = "teacher class"
+    class_name = fields.Char(string="Title", required=True)
